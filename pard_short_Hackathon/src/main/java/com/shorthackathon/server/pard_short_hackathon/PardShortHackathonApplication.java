3ba049824c48fead942405e6795fe66867f040fb
노현경 <pause13818@gmail.com>
2025-11-21 21:59:24 +0900

    first push

---
+++ pard_short_Hackathon/src/main/java/com/shorthackathon/server/pard_short_hackathon/PardShortHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.shorthackathon.server.pard_short_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PardShortHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PardShortHackathonApplication.class, args);
+    }
+
+}
